8ca673048ee50f2be4a9fbecdf5ed3e1d339afae
Thomas Kopton <tkopton@vmware.com>
2017-10-21 16:21:13 +0200

    First Spring MVC test application

---
+++ springmvc-01/src/main/java/com/thomaskopton/springmvc01/Springmvc01Application.java
@@ -0,0 +1,12 @@
+package com.thomaskopton.springmvc01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springmvc01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springmvc01Application.class, args);
+	}
+}
