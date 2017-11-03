6ea6f3fc8dbaf19e8402bc9616866fb0d6a01d10
Thomas Kopton <tkopton@vmware.com>
2017-11-03 18:01:12 +0100

    Config module configured

---
+++ springmvc-01-config/src/main/java/com/thomaskopton/springmvc01config/DispatcherServletInitializer.java
@@ -0,0 +1,29 @@
+package com.thomaskopton.springmvc01config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+/**
+ * @author tkopton
+ *
+ */
+public class DispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer{
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return new Class[] {
+				RootApplicationContextConfig.class
+		};
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return new Class[] {
+				WebApplicationContextConfig.class
+		};
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return new String[] { "/" }; //Every incoming request will be handled by this Dispatcher
+	}
+}
---
+++ springmvc-01-config/src/main/java/com/thomaskopton/springmvc01config/SecurityWebApplicationInitializer.java
@@ -0,0 +1,7 @@
+package com.thomaskopton.springmvc01config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+ 
+}
