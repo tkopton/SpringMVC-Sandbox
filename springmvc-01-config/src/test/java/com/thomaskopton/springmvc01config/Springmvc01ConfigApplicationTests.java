ffaa38515ac17eac734d8384f869c5a2e5f65fb8
Thomas Kopton <tkopton@vmware.com>
2017-11-03 17:58:00 +0100

    adding config module

---
+++ springmvc-01-config/src/test/java/com/thomaskopton/springmvc01config/Springmvc01ConfigApplicationTests.java
@@ -0,0 +1,16 @@
+package com.thomaskopton.springmvc01config;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Springmvc01ConfigApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
